95b6b82630ed9911471c0d50021cfa821405ed3c
lenard-villestas <lenard.villestas@outlook.com>
2023-01-31 13:21:15 -0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,16 +29,6 @@ function Header() {
           <li className="nav-item">
             <a className="nav-link mx-2" href="#">About Us</a>
           </li>
-          {/*<li className="nav-item dropdown">
-            <a className="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Company
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a className="dropdown-item" href="#">Blog</a></li>
-              <li><a className="dropdown-item" href="#">About Us</a></li>
-              <li><a className="dropdown-item" href="#">Contact us</a></li>
-            </ul>
-         </li>*/}
 
         </ul>
       </div>
