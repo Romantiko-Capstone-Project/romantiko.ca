caf9f4a1abda52519a882525dd8288a68e9cabf7
muhammad3452 <113657089+mtayyab556@users.noreply.github.com>
2023-02-07 15:33:42 -0700

    Added css functionality

--- src/components/LoginPage/Loginpage.jsx
+++ src/components/LoginPage/Loginpage.jsx
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "../../../styles/Home.module.css";
+import styles from "../../../styles/LoginPage.module.css";
 
 function LoginPage() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', { username, password })
-      console.log(response.data)
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        { username, password }
+      );
+      console.log(response.data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit} classname={styles.loggingin}>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Email"/><br/><br/>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/><br/><br/>
-      <input type="submit" value="Login"/>
-    </form>
-  )
+    <div>
+      <div className={styles.wrapper}>
+        <input
+          className={styles.input}
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          className={styles.input}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className={styles.button} onClick={handleSubmit}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
