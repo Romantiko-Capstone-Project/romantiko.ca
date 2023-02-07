0bad89c03446ef8945c313c6abc922d5dc525003
Sowmithra Suresh Kumar <sowmithra.sureshkumar@edu.sait.ca>
2023-02-07 13:38:27 -0700

    Completed basic form for client information.

---
+++ pages/api/booking/ClientInformation.jsx
@@ -0,0 +1,32 @@
+import { styles } from '@material-ui/pickers/views/Calendar/Calendar';
+import React from 'react';
+
+const ClientInformation = () => {
+    return (
+        <div className={styles.container}>
+            <form className={styles.form}>
+                <label>
+                    Name: <br />
+                    <input type="text" name="name"></input>
+                </label> <br />
+                <label> 
+                    Email: <br />
+                    <input type="email" name="email"></input>
+                </label> <br />
+                <label>
+                    Phone Number: <br />
+                    <input type="tel" name="phonenumber"></input>
+                </label> <br />
+                <label>
+                    Add a message: <br />
+                    <textarea>
+                        Notes...
+                    </textarea>
+                </label>
+                
+            </form>
+        </div>
+    );
+}
+
+export default ClientInformation;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import ServiceList from './component/ServiceList'
-import BarberList from './component/BarberList'
+import ServiceList from './api/booking/ServiceList'
+import BarberList from './api/booking/BarberList'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/@mui/x-date-pickers/AdapterDayjs/index.js';
 import "react-datepicker/dist/react-datepicker.css";
-import MuiPicker from './component/MuiPicker'
+import MuiPicker from './api/booking/MuiPicker';
+import ClientInformation from './api/booking/ClientInformation';
 
 export default function Home() {
   
   return (
     <>
     <MuiPicker />
+    <ClientInformation />
     </>
   );
 }
