4eea2533d7feba45547cc7a309298371d23ed14d
Sowmithra Suresh Kumar <sowmithra.sureshkumar@edu.sait.ca>
2023-02-07 13:42:10 -0700

    Moved files to booking api folder.

--- pages/api/booking/index.js
+++ pages/api/booking/index.js
@@ -1,6 +1,16 @@
 import dbConnect from "../../../util/mongo";
 import Booking from "../../../models/booking"
 
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import ServiceList from './api/booking/ServiceList'
+import BarberList from './api/booking/BarberList'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/@mui/x-date-pickers/AdapterDayjs/index.js';
+import "react-datepicker/dist/react-datepicker.css";
+import MuiPicker from './api/booking/MuiPicker';
+import ClientInformation from './api/booking/ClientInformation';
 
 export default async function handler(req, res){
     const {method} = req;
--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,6 @@ export default function Home() {
   
   return (
     <>
-    <MuiPicker />
-    <ClientInformation />
     </>
   );
 }
