5914aae077409710eb70d525613584ecbdda3f9b
lenard-villestas <lenard.villestas@outlook.com>
2023-02-14 14:27:57 -0700

    update next version
    updates next version dependency
    andupdates footer

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import banner from '../../../public/img/aboutus/banner-aboutus.jpg';
 
-import Image from 'next/image';
 const AboutUs = () => {
     return (
-        <section class="py-5">
-            <div class="text-center my-5">
-                <Image class="img-fluid rounded-circle mb-4" src= '/img/aboutus/banner-aboutus.jpg' layout='fill' alt="..." />
+        <section className="py-5">
+            <div className="text-center my-5">
+            <img className="img-fluid rounded-circle mb-4" src={banner} alt="..." />
                 
 
             </div>
-            <div class="container my-5">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
+            <div className="container my-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6">
                         <h2>We are..</h2>
-                        <p class="lead">A locally owned business, in the heart if Airdrie</p>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non malesuada ipsum, in dictum lacus. Morbi mattis, felis ut tincidunt maximus, nisl diam maximus tortor, vitae ullamcorper eros magna vitae justo. Sed eu felis arcu. Nulla tincidunt felis eget nisl vulputate, nec pharetra tellus tristique. Praesent nec ante eu nisl vehicula aliquet quis a ipsum. Duis in bibendum risus. Vivamus sit amet tincidunt nisl. Nam rutrum nulla rhoncus turpis lobortis pulvinar. Sed molestie libero sed orci molestie, sit amet commodo velit aliquet. Proin in augue rutrum, scelerisque nisi eu, maximus tellus. Fusce cursus semper lectus. Nulla suscipit metus eu purus tincidunt dictum. Ut sagittis diam sit amet faucibus posuere.</p>
+                        <p className="lead">A locally owned business, in the heart if Airdrie</p>
+                        <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non malesuada ipsum, in dictum lacus. Morbi mattis, felis ut tincidunt maximus, nisl diam maximus tortor, vitae ullamcorper eros magna vitae justo. Sed eu felis arcu. Nulla tincidunt felis eget nisl vulputate, nec pharetra tellus tristique. Praesent nec ante eu nisl vehicula aliquet quis a ipsum. Duis in bibendum risus. Vivamus sit amet tincidunt nisl. Nam rutrum nulla rhoncus turpis lobortis pulvinar. Sed molestie libero sed orci molestie, sit amet commodo velit aliquet. Proin in augue rutrum, scelerisque nisi eu, maximus tellus. Fusce cursus semper lectus. Nulla suscipit metus eu purus tincidunt dictum. Ut sagittis diam sit amet faucibus posuere.</p>
                     </div>
                 </div>
             </div>
