d99f57f4072fd50036c5e3bef9a9f14d257b2333
Sowmithra Suresh Kumar <sowmithra.sureshkumar@edu.sait.ca>
2023-02-16 13:05:26 -0700

    Hard coding in different hair products.

--- src/components/shop/ShopCard.jsx
+++ src/components/shop/ShopCard.jsx
@@ -5,15 +5,20 @@ import styles from "/styles/ShopCard.module.css"
 const ShopCard = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <Image src="/img/service.png" alt="" width="50" height="50"/>
-                <h1 className={styles.title}>Service Name</h1>
-
-                <div className={styles.description}>
-                    <span className={styles.time}>30 minutes</span>
-                    <span className={styles.price}>$20+gst</span>
+            <div className={styles.left}>
+                <div className={styles.wrapper}>
+                    <h1 className={styles.title}>Service Name</h1>
+                    <div className={styles.info}>
+                        <span className={styles.description}>30 minutes</span>
+                        <span className={styles.price}>$20+gst</span>
+                    </div>
                 </div>
             </div>
+                <div className={styles.right}>
+                    <div className={styles.imgContainer}>
+                        <Image src="/img/service.png" alt="" width="100" height="100"/>
+                    </div>
+                </div>  
         </div>
     );
 }
--- src/components/shop/ShopList.jsx
+++ src/components/shop/ShopList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "/styles/ShopList.module.css";
 import ShopCard from "./ShopCard";
+import Image from "next/image";
 
 const ShopList = () => {
     return (
@@ -13,12 +14,139 @@ const ShopList = () => {
             </p>
 
             <div className={styles.wrapper}>
-                <ShopCard />
-                <ShopCard />
-                <ShopCard />
-                <ShopCard />
-                <ShopCard />
-                <ShopCard />
+                
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Pomade</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair styling product that provides a shiny, slick look and a strong hold.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Hair Gel</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair styling product that provides a firm hold and shiny finish.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Hair Spray</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair styling product that provides a long-lasting hold and helps control frizz.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Hair Wax</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair styling product that provides a matte finish and a strong hold, ideal for short haircuts.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Hair Tonic</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A scalp treatment product that helps strengthen hair and promote healthy growth.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Shampoo</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair care product used to cleanse the scalp and hair of dirt, oil, and product buildup.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
+{/*BREAK */}
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardLeft}>
+                        <div className={styles.cardWrapper}>
+                            <h1 className={styles.cardTitle}>Conditioner</h1>
+                            <div className={styles.cardInfo}>
+                                <span className={styles.cardDescription}>
+                                    A hair care product used to moisturize and detangle hair after shampooing.
+                                </span>
+                                <span className={styles.cardPrice}>$20+gst</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cardRight}>
+                        <div className={styles.cardImgContainer}>
+                            <Image src="/img/service.png" alt="" width="100" height="100"/>
+                        </div>
+                    </div>  
+                </div>
             </div>
         </div>
     );
