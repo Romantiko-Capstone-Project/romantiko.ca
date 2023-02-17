902666da793f969897e6dc2daca8a7e93af60931
muhammad3452 <113657089+mtayyab556@users.noreply.github.com>
2023-02-16 17:06:12 -0700

    Added login page (By Muhammad Tayyab)

--- src/components/LoginPage/Loginpage.jsx
+++ src/components/LoginPage/Loginpage.jsx
@@ -1,28 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "../../../styles/Home.module.css";
+import styles from "../../../styles/LoginPage.module.css";
 
 function LoginPage() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMess,setErrorMessage] = useState("");
+  const [error, setError] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', { username, password })
-      console.log(response.data)
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        { username, password }
+      );
+      console.log(response.data);
     } catch (error) {
-      console.error(error)
+      setError(true);
+      setErrorMessage("Incorrect username or password.");
+      console.error(error);
     }
-  }
+
+    
+
+  };
+
 
   return (
-    <form onSubmit={handleSubmit} className={styles.loggingin}>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Email"/><br/><br/>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/><br/><br/>
-      <input type="submit" value="Login"/>
-    </form>
-  )
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h4 style={{"text-align": "center", "margin-top": "8px", "color": "black"}}>Staff Login</h4>
+
+        <div className="form-floating mb-3 mt-3">
+        <input
+          className= "form-control"
+          type="text"
+          id="email"
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email"
+          style={{"background-color": "#EBECF0"}}
+        /><label htmlFor="email" className={styles.stuff}>Email</label>
+        </div>
+
+        <div className="form-floating mt-3 mb-3">
+        <input
+          className="form-control"
+          type="password"
+          id="pwd"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          style={{"background-color": "#EBECF0"}}
+        /><label htmlFor="pwd" className={styles.stuff}>Password</label>
+        </div>
+
+        {error && <div className="error-message" style={{"text-align": "center", "color": "red"}}>{errorMess}</div>}
+
+        <div>
+        <button className={styles.button} onClick={handleSubmit}>
+          Login
+        </button><br/>
+        
+        <a href="https://www.google.com/?hl=en-US" className={styles.mylink}>Forgot password?</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
