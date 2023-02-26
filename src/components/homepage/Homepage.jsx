a9e04e36098522d683e586db53108191ec029fc4
lenard-villestas <lenard.villestas@outlook.com>
2023-02-26 01:22:43 -0700

    homepage new section
    google map and google review section added
    responsive styling integrated

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import Featured from "./Featured";
+import CardSlide from "./CardSlide";
+
 
 const Homepage = () => {
   return (
@@ -21,6 +22,23 @@ const Homepage = () => {
         at prices that won't break the bank.
          Whether you need a classic cut or a modern style, our team will ensure you leave looking and feeling your best.
          Join our community of locals and experience the best in affordable grooming.</p>
+      </div>
+
+      <div className="googleSection">
+          
+      <div className="map">
+
+      <a href="https://www.google.com/maps/place/Romantiko+Barber+Shop/@51.2911508,-114.0028717,338m/data=!3m1!1e3!4m6!3m5!1s0x53715f25208d2603:0xe5655ce1b5340234!8m2!3d51.2910039!4d-114.0030376!16s%2Fg%2F11qgyl2v4v" target="_blank">
+        <img src="/img/homepage/map.JPG"></img>
+      </a>
+      </div>
+
+      <div className="review">
+        <CardSlide/>
+
+      </div>
+
+
       </div>
     </>
   );
