7f90f72d287336a1eafbbc4eec6acedc42ddc9c3
lenard-villestas <lenard.villestas@outlook.com>
2023-02-25 18:53:52 -0700

    homepage update
    adds hero message

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Featured from "./Featured";
-import styles from '../../../styles/Home.module.css';
+
 const Homepage = () => {
   return (
     <>
@@ -9,7 +9,7 @@ const Homepage = () => {
       <video loop muted autoPlay style={{
         position: "relative",
         width: "100%",
-        height: "95vh",
+        height: "100%",
         objectFit: "cover",
         left: 0,
         top: 0,
@@ -19,7 +19,18 @@ const Homepage = () => {
         <source src="/videos/bgloop.mp4" type="video/mp4" />
       </video>
 
-      
+      <div className="hero">
+      <h1 style={{fontWeight: "900"}}>Airdrie's Neighborhood Barbershop: Affordable Prices, Exceptional Service</h1>
+
+      <Link href={"/booking"}>
+        <button>Book a haircut now</button>
+      </Link>
+
+      <p>Local charm meets professional quality. Our expert barbers are dedicated to providing you with the finest grooming experience 
+        at prices that won't break the bank.
+         Whether you need a classic cut or a modern style, our team will ensure you leave looking and feeling your best.
+         Join our community of locals and experience the best in affordable grooming.</p>
+      </div>
     </>
   );
 };
