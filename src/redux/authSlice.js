9669ef1354c80a6c595043ad91202ce5792ade86
lenard-villestas <lenard.villestas@outlook.com>
2023-03-03 13:23:28 -0700

    Update authSlice.js
    added role state

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -4,16 +4,23 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: {
       loggedIn: false,
+      role: ""
     },
     reducers: {
       login: (state) => {
         state.loggedIn = true;
+        state.role = "staff";
       },
       logout: (state) => {
         state.loggedIn = false;
+        state.role = "";
+      },
+      adminLogin: (state) => {
+        state.loggedIn = true;
+        state.role = "admin";
       },
     },
   });
   
-  export const { login, logout } = authSlice.actions;
+  export const { login, logout, adminLogin } = authSlice.actions;
   export default authSlice.reducer;
