eb499e23e56e7410c719dc2cbd33f976efdbea37
Khanh Nguyen <118481086+khanhn11@users.noreply.github.com>
2023-03-21 14:00:02 -0600

    models schema updated

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,38 +1,42 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Schedule = new mongoose.Schema({
-  day: {
-    type: String,
-    enum: [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ],
-    required: true,
+const Schedule = new Schema(
+  {
+    day: {
+      type: String,
+      enum: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      required: true,
+    },
+    staff: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Staff",
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    booking: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
+      required: true,
+    },
   },
-  staff: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Staff",
-    required: true,
-  },
-  startTime: {
-    type: String,
-    required: true,
-  },
-  endTime: {
-    type: String,
-    required: true,
-  },
-  booking: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Booking",
-    required: true,
-  }
-});
+  { timestamps: true }
+);
 
 module.exports =
   mongoose.models.Schedule || mongoose.model("Schedule", Schedule);
--- models/booking.js
+++ models/booking.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Booking = new mongoose.Schema(
+const Booking = new Schema(
   {
     startTime: {
       type: String,
