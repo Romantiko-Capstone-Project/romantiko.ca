2b10d8ab711a828c4fdd2e1fb1c799d33417d08c
Patrick O <patrick.orihuela@gmail.com>
2023-03-26 15:23:18 -0600

    GalleryTab Update

--- src/components/admin/GalleryTab.jsx
+++ src/components/admin/GalleryTab.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Image from "next/image";
+import styles from "../../../styles/GalleryTab.module.css";
 
 const GalleryTab = () => {
   const [file, setFile] = useState(null);
   const [msg, setMsg] = useState(false);
+  const [images, setImages] = useState([]);
 
   const handleCreate = async () => {
     const data = new FormData();
@@ -28,23 +31,53 @@ const GalleryTab = () => {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/api/gallery");
+        if (JSON.stringify(images) !== JSON.stringify(data)) {
+          setImages(data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchImages();
+  }, [images]);
+
   return (
     <div>
-      <h1>Gallery Form</h1>
-      <br></br>
-      <div>
-        <label>Choose an image</label>
-        <br></br> 
-        {/* Get rid of br and uses flex-box. */}
-        <input
-          type="file"
-          onChange={(e) => setFile(e.target.files[0])}
-          onClick={() => setMsg(false)}
-        
-        />
-        {msg && <h3>The image has been succesfully uploaded.</h3>}
+      <div className="update-form">
+        <h1>Gallery Form</h1>
+        <br></br>
+        <div>
+          <label>Choose an image</label>
+          <br></br>
+          {/* Get rid of br and uses flex-box. */}
+          <input
+            type="file"
+            onChange={(e) => setFile(e.target.files[0])}
+            onClick={() => setMsg(false)}
+          />
+          {msg && <h3>The image has been succesfully uploaded.</h3>}
+        </div>
+        <button onClick={handleCreate}>Upload</button>
       </div>
-      <button onClick={handleCreate}>Upload</button>
+      <div className={styles.container}>
+        <div className={styles.imagesContainer}>
+          {images.map((image) => (
+            <div className={styles.imgContainer} key={image._id}>
+              <Image
+                src={image.img}
+                alt="Haircut img not found"
+                width="500"
+                height="500"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
