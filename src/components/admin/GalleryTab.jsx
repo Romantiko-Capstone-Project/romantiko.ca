f784681e7954957f8edbc72cd1fe0b66818dc5c8
muhammad3452 <113657089+mtayyab556@users.noreply.github.com>
2023-03-31 12:17:27 -0600

    Added a nicer delete button, along with some UI for the upload button

--- src/components/admin/GalleryTab.jsx
+++ src/components/admin/GalleryTab.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
 import styles from "../../../styles/GalleryTab.module.css";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const GalleryTab = () => {
   const [file, setFile] = useState(null);
@@ -17,16 +18,22 @@ const GalleryTab = () => {
         "https://api.cloudinary.com/v1_1/dyk9lstek/image/upload",
         data
       );
-      // console.log(uploadRes.data);
       const { url } = uploadRes.data;
       const newGallery = {
         img: url,
       };
-      console.log(newGallery);
 
       await axios.post("http://localhost:3000/api/gallery", newGallery);
-      // setClose(true);
       setMsg(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleRemove = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/api/gallery/${id}`);
+      setImages(images.filter((image) => image._id !== id));
     } catch (err) {
       console.log(err);
     }
@@ -36,9 +43,7 @@ const GalleryTab = () => {
     const fetchImages = async () => {
       try {
         const { data } = await axios.get("http://localhost:3000/api/gallery");
-        if (JSON.stringify(images) !== JSON.stringify(data)) {
-          setImages(data);
-        }
+        setImages(data);
       } catch (err) {
         console.log(err);
       }
@@ -47,20 +52,16 @@ const GalleryTab = () => {
   }, [images]);
 
   return (
-    <div>
-      <div className="update-form">
-        <h1>Gallery Form</h1>
-        <br></br>
+    <div className={styles.mainContainer1}>
+      <div className={styles.formContainer}>
         <div>
           <label>Choose an image</label>
           <br></br>
-          {/* Get rid of br and uses flex-box. */}
           <input
             type="file"
             onChange={(e) => setFile(e.target.files[0])}
             onClick={() => setMsg(false)}
           />
-          {msg && <h3>The image has been succesfully uploaded.</h3>}
         </div>
         <button onClick={handleCreate}>Upload</button>
       </div>
@@ -74,6 +75,7 @@ const GalleryTab = () => {
                 width="205"
                 height="200"
               />
+              <button className={styles.deleteButton}><DeleteIcon onClick={() => handleRemove(image._id)}/></button>
             </div>
           ))}
         </div>
@@ -83,90 +85,3 @@ const GalleryTab = () => {
 };
 
 export default GalleryTab;
-
-// import Dropzone from "react-dropzone";
-// import Masonry from "react-masonry-css";
-// import { FaTrash } from "react-icons/fa";
-// import styled from "styled-components";
-
-// const breakpoints = {
-//   default: 3,
-//   1100: 3,
-//   700: 2,
-//   500: 1,
-// };
-
-// function GalleryTab() {
-//   const [images, setImages] = useState([]);
-
-//   const handleDrop = (acceptedFiles) => {
-//     setImages([...images, ...acceptedFiles]);
-//   };
-
-//   const handleRemove = (index) => {
-//     const newImages = [...images];
-//     newImages.splice(index, 1);
-//     setImages(newImages);
-//   };
-
-//   return (
-//     <div>
-//       <Dropzone onDrop={handleDrop}>
-//         {({ getRootProps, getInputProps }) => (
-//           <DropzoneWrapper {...getRootProps()}>
-//             <input {...getInputProps()} />
-//             <p>Drag and drop some files here, or click to select files</p>
-//           </DropzoneWrapper>
-//         )}
-//       </Dropzone>
-//       <Masonry breakpointCols={breakpoints} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
-//         {images.map((image, index) => (
-//           <ImageWrapper key={index}>
-//             <RemoveButton onClick={() => handleRemove(index)}>
-//               <FaTrash />
-//             </RemoveButton>
-//             <Image src={URL.createObjectURL(image)} alt={`Image ${index}`} />
-//           </ImageWrapper>
-//         ))}
-//       </Masonry>
-
-//     </div>
-//   );
-
-// }
-
-// const DropzoneWrapper = styled.div`
-//   border: 2px dashed #ccc;
-//   padding: 50px;
-//   text-align: center;
-//   margin-top: 50px;
-//   margin-left: 50px;
-// `;
-
-// const ImageWrapper = styled.div`
-//   position: relative;
-// `;
-
-// const RemoveButton = styled.button`
-//   position: absolute;
-//   top: 0;
-//   right: 0;
-//   background-color: #ff3b3b;
-//   color: white;
-//   border: none;
-//   font-size: 1rem;
-//   padding: 0.5rem;
-//   border-radius: 0.5rem;
-//   cursor: pointer;
-//   z-index: 10;
-// `;
-
-// const Image = styled.img`
-//   display: block;
-//   width: 100%;
-//   height: auto;
-//   border-radius: 0.5rem;
-//   margin-bottom: 1rem;
-// `;
-
-// export default GalleryTab;
